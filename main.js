d1a2549277d22ad580d72e0368d6bc70451294e6
mariam502 <marrakbi@gmail.com>
2022-11-09 21:46:03 +0300

    add lines in html and js

--- main.js
+++ main.js
@@ -1,22 +1,73 @@
 
-const elements ={
-  currentCalculation:document.querySelector("#current-calculation"),
-  numbers:document.querySelectorAll(".numbers"),
-  clear:document.querySelector("#clear")
-};
-
+const currentCalculation=document.querySelector("#current-calculation");
+const clear=document.querySelector("#clear");
+const numbers=document.querySelectorAll(".numbers");
+const point=document.querySelector("#point");
+const sign=document.querySelectorAll(".sign");
+const equal=document.querySelector("#equal");
 
 function numberClicked(e){
   const button=e.target;
   const number= button.textContent;
-  elements.currentCalculation.textContent+=number;
+  currentCalculation.textContent+=number;
 }
 
-elements.numbers.forEach((element)=> {
+
+numbers.forEach((element)=> {
   element.addEventListener("click", numberClicked);
 });
 
 
-elements.clear.addEventListener("click", () => {
-  elements.currentCalculation.textContent = "" ;
+clear.addEventListener("click", () => {
+  currentCalculation.textContent = "" ;
+});
+
+
+point.addEventListener("click", () => {
+  const LastNumber=currentCalculation.textContent.slice(-1);
+  if (LastNumber!=="."){
+  currentCalculation.textContent += "." ;}
+});
+
+numbers.forEach((element)=> {
+  element.addEventListener("click", numberClicked);
+});
+
+
+
+sign.forEach((element)=> {
+  element.addEventListener("click", numberClicked);
+});
+
+
+equal.addEventListener("click", () => {
+
+  const text =currentCalculation.textContent;
+  let result;
+  let first;
+  let second;
+  let currentSign;
+  for (const letter of text.split("")){
+
+    if (first === undefined){
+      first=letter;
+
+    } else if(currentSign === undefined){
+      currentSign=letter;
+
+    } else if(second === undefined){
+      second = letter;
+    }
+
+    if(first !== undefined &&
+       currentSign !== undefined&&
+       second !== undefined){
+         currentCalculation.textContent={
+           "+":parseInt(first)+parseInt(second),
+           "*":parseInt(first)*parseInt(second),
+           "/":parseInt(first)/parseInt(second),
+           "-":parseInt(first)-parseInt(second),
+         }[currentSign];
+       }
+  }
 });
